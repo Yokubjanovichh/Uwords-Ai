f7e06ac9e57a4c4d52a62eb72f53cd3478b173ff
Yokubjanovich <yokubjanovich@gmail.com>
2025-03-25 21:14:05 +0500

    finish first page header

---
+++ app/components/navbar.js
@@ -0,0 +1,58 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
+export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    console.log("hekki");
+    setMenuOpen((prev) => !prev);
+    console.log(menuOpen);
+  }
+
+  return (
+    <nav>
+      <div className="navWrapper">
+        <a href="#">
+          <img src="/icons/navbarLogo.svg" alt="MainLogo" />
+        </a>
+        <div className="navMenu">
+          <Link href="/">О проекте</Link>
+          <Link href="/">инвесторам</Link>
+          <Link href="/">школам</Link>
+          <Link href="/">учащимся</Link>
+          <Link href="/">Блог</Link>
+        </div>
+        <button className="navWrapperButton">скачать</button>
+        <img
+          src="/icons/mobHumbergerIcon.svg"
+          alt="menu"
+          onClick={toggleMenu}
+          className="mobMenuBtn"
+        />
+      </div>
+
+      <div className={`mobMenu ${menuOpen ? "visibleMobMenu" : ""}`}>
+        <div className="mobMenuExit">
+          <a href="#">
+            <img src="/icons/navbarLogo.svg" alt="MainLogo" />
+          </a>
+          <img
+            onClick={toggleMenu}
+            src="/icons/close-menu.svg"
+            alt="close menu"
+          />
+        </div>
+        <div className="navMenu">
+          <Link href="/">О проекте</Link>
+          <Link href="/">инвесторам</Link>
+          <Link href="/">школам</Link>
+          <Link href="/">учащимся</Link>
+          <Link href="/">Блог</Link>
+        </div>
+        <button className="navWrapperButton">скачать</button>
+      </div>
+    </nav>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,58 @@
+export default function Hage() {
+  return (
+    <div className="aboutProjectWrapper">
+      <div className="aboutPrHeader">
+        <p className="aboutPrHeaderTitle">Изучай языки c</p>
+        <div className="aboutPrHeaderBody">
+          <div className="aboutPrHeaderBodyLeftWrapper">
+            <p className="aboutPrHeaderTitle">Uwords AI</p>
+            <div className="aboutPrHeaderBodyLeft">
+              <img src="/images/aboutProject1.webp" alt="image" />
+              <div className="aboutPrHeaderBodyLeftRate">
+                <p>Учите слова легко и эффективно с нашим умным ИИ.</p>
+                <div className="aboutPrHeaderBodyLeftStar">
+                  <div className="aboutPrHeaderBodyLeftStars">
+                    <p className="aboutPrHeaderBodyLeftStarsPinkText">
+                      4.9 rating
+                    </p>
+                    <div>
+                      <img src="/icons/star.svg" alt="icon" />
+                      <img src="/icons/star.svg" alt="icon" />
+                      <img src="/icons/star.svg" alt="icon" />
+                      <img src="/icons/star.svg" alt="icon" />
+                      <img src="/icons/star.svg" alt="icon" />
+                    </div>
+                  </div>
+                  <div className="aboutPrHeaderBodyLeftUserPurpose">
+                    <div>
+                      <img src="/images/aboutUser1.webp" alt="image" />
+                      <img
+                        src="/images/aboutUser2.webp"
+                        alt="image"
+                        className="userToLeft"
+                      />
+                      <img
+                        src="/images/aboutUser3.webp"
+                        alt="image"
+                        className="userToLeft"
+                      />
+                    </div>
+                    <div>
+                      <p className="aboutPrHeaderBodyLeftStarsPinkText">
+                        +102 k
+                      </p>
+                      <p>пользователей нам доверяют</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="phoneWrapper">
+            {/* <img src="/images/aboutPhone.webp" alt="image" /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
